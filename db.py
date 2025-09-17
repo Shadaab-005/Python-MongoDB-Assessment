6a8dedf572e61dedcb9f8908dae535f57358adfa
Shadaab Ahmad <101640675+Shadaab-005@users.noreply.github.com>
2025-09-17 12:24:38 +0530

    Update db.py

--- db.py
+++ db.py
@@ -6,7 +6,8 @@ DB_NAME = "assessment_db"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 employees_collection = db["employees"]
+users_collection = db["users"]   
 
-# Creating unique index on employee_id
 async def init_db():
     await employees_collection.create_index("employee_id", unique=True)
+    await users_collection.create_index("username", unique=True)  
