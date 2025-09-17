a774c14f3e0abba3b5aeb8b2160523610bdee8e1
Shadaab Ahmad <101640675+Shadaab-005@users.noreply.github.com>
2025-09-17 13:41:25 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from db import users_collection
+
+SECRET_KEY = "3Nf8u9-7N8jKjQvOeOJ3v5mA5PqkbnrKJ7T53j9Wc4s"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+async def authenticate_user(username: str, password: str):
+    user = await users_collection.find_one({"username": username})
+    if not user or not verify_password(password, user["hashed_password"]):
+        return None
+    return user  
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await users_collection.find_one({"username": username})
+    if user is None:
+        raise credentials_exception
+    return username
